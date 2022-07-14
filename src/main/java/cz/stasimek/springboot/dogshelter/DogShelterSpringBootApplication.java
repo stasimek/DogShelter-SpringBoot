4edea14845a4c9f86c61e398da9884d78ea5ac8f
Stanislav Simek <stasimek@gmail.com>
2022-07-15 00:44:32 +0200

    Spring Boot application generated by Spring Initializr - https://start.spring.io

---
+++ src/main/java/cz/stasimek/springboot/dogshelter/DogShelterSpringBootApplication.java
@@ -0,0 +1,13 @@
+package cz.stasimek.springboot.dogshelter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogShelterSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogShelterSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/stasimek/springboot/dogshelter/DogShelterSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.stasimek.springboot.dogshelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogShelterSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
